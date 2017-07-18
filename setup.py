16dbd0b809c9ed7c487d66c59ff31542c14007fa
Dan Higgins <daniel.higgins@yahoo.com>
2017-07-18 11:43:18 -0400

    minor updates

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ask_amy',
-      version='0.2.8',
+      version='0.2.9',
       description='Python framework for Alexa Skill development',
       url='https://github.com/dphiggs01/ask_amy',
       author='Dan Higgins',
