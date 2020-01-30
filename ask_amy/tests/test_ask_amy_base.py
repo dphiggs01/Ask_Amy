af0351154705ec861256b7877add2a93bce62db4
Dan Higgins <daniel.higgins@yahoo.com>
2020-01-30 12:29:14 -0500

    updated test base

--- ask_amy/tests/test_ask_amy_base.py
+++ ask_amy/tests/test_ask_amy_base.py
@@ -1,6 +1,7 @@
 import unittest
 import logging
 import os
+import sys
 import json
 
 
@@ -9,6 +10,11 @@ class TestCaseASKAmy(unittest.TestCase):
     """
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+    path = sys.path
+    test_data_dir = "{}/data/".format(os.path.dirname(__file__))
+    if test_data_dir not in path:
+        path.insert(0, test_data_dir)
+
     # Create and environment variable ASK_AMY_LOGGING to specify a logging directory
     path = os.getenv('ASK_AMY_LOGGING_DIR', os.path.expanduser('~')) + os.sep
     hdlr = logging.FileHandler(path + 'logfile.log')
@@ -18,12 +24,9 @@ class TestCaseASKAmy(unittest.TestCase):
 
     # Helper functions
     def load_json_file(self, file_name):
-        path_to_test_data='../data'
-        if not os.path.exists(path_to_test_data):
-            # Assume we are running tests from project root
-            path_to_test_data = "{}/ask_amy/tests/data".format(os.getcwd())
-
-        file_path = "{}/{}".format(path_to_test_data,file_name)
+        test_script_location = os.path.dirname(__file__)
+        file_path = "{}/data/{}".format(test_script_location,file_name)
+        print("file_path {}".format(file_path))
         file_ptr_r = open(file_path, 'r')
         json_data = json.load(file_ptr_r)
         file_ptr_r.close()
