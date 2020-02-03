891dd3c6f3fd39236deed346f301ee4b7cd5eccd
Dan Higgins <daniel.higgins@yahoo.com>
2020-02-03 12:29:23 -0500

    updated MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
 
 setup(name='ask_amy',
-      version='0.4.6',
+      version='0.4.5',
       description='Python framework for Alexa Skill development',
       url='https://github.com/dphiggs01/ask_amy',
       author='Dan Higgins',
       author_email='daniel.higgins@yahoo.com',
       license='MIT',
-      packages=['ask_amy', 'ask_amy.cli', 'ask_amy.cli.code_gen', 'ask_amy.core', 'ask_amy.services','ask_amy.database', 'ask_amy.state_mgr', 'ask_amy.utilities'],
-      package_data={'ask_amy': ['cli/code_gen/templates/*.tmpl']},
 
+      packages=['ask_amy', 'ask_amy.cli', 'ask_amy.cli.code_gen', 'ask_amy.cli.code_gen.templates',
+                'ask_amy.core', 'ask_amy.services','ask_amy.database',
+                'ask_amy.state_mgr', 'ask_amy.utilities'],
+      # Add template files to MANIFEST!!
       test_suite='ask_amy.tests',
       entry_points={
           'console_scripts': ['ask-amy-cli=ask_amy.cli.command_line:main'],
